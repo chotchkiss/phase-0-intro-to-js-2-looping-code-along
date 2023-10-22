d5baee56ef9b595730cd6f2c16bb025190607ecf
Cheyenne Hotchkiss <cheycheymh@gmail.com>
2023-10-22 06:20:11 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = (["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length - 0; i++) {
+      messages.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+    }
+    return messages;
+  }
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
